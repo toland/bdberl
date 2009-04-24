2d2fd9476947ed953ec0c6565c2066f6c25d9379
Dave Smith <dsmith@thehive.com>
2009-04-24 09:41:40 -0600

    Remove unnecessary mkdir in buildlib.sh

--- c_src/buildlib.sh
+++ c_src/buildlib.sh
@@ -22,7 +22,6 @@ tar -xzf db-${DB_VER}.tar.gz && \
 ##(cd db-${DB_VER} && patch -p0 < ../bdb-align.patch )  && \
 (cd db-${DB_VER}/build_unix && \
     ../dist/configure --prefix=$WORKDIR --disable-shared --enable-o_direct --with-pic && make && ranlib libdb-*.a && make install) && \
-    mkdir -p $TARGETDIR/utils && \
     rm -rf db-${DB_VER}
 
 
